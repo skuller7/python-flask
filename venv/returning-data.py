c3ded4c0153c2b0e3f124415b2b727c6d4f80ce1
Uros Zigic <ukizigic@gmail.com>
2025-12-01 18:08:16 +0100

    added new content

--- venv/returning-data.py
+++ venv/returning-data.py
@@ -12,3 +12,27 @@ def get_users():
     response.headers['Content-Type'] = "application/json"
     
     return response
+
+@app.route('/article1')
+def article1():
+    return render_template(
+        'article.html',
+        title='My First Article',
+        content="Thank you for reading! this is my first article"
+    )
+
+@app.route('/article2')
+def article2():
+    return render_template(
+        'article.html',
+        title='My Second Article',
+        content="Thank you for reading! this is my second article"
+    )
+
+@app.route('/article3')
+def article3():
+    return render_template(
+        'article.html',
+        title='My Third Article',
+        content="Thank you for reading! this is my third article"
+    )
